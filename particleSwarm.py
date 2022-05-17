2d9bc88161fea680df8ae361cb512e4aaa757c2a
Marcel Martinšek <mm0445@student.uni-lj.si>
2022-05-17 23:38:16 +0200

    functions.py: boundary arrays particleSwarm: started

---
+++ particleSwarm.py
@@ -0,0 +1,24 @@
+from mimetypes import init
+import numpy as np
+class ParticleSwarm:
+    def __init__(self, function, pop=50,w_limits=[0.9,0.4],c1=1,c2=1):
+        self.obj_fun = function
+        self.population = pop
+        self.w_limits = w_limits
+        self.c1 = c1
+        self.c2 = c2
+
+    def init_swarm(self,dim,bounds):
+        swarm = np.random.uniform(low=bounds[:,0],high=bounds[:,1],size=(self.population,dim))
+        return swarm
+
+    def optimize(self, par, bounds):
+        swarm = self.init_swarm(len(par),bounds)
+        pbest = np.zeros(swarm)
+        pbest_value = np.zeros(swarm.shape[0])
+        print(swarm)
+
+
+        return swarm
+        
+
